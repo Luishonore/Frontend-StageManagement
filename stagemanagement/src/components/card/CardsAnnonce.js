2218273b27021c3249590d27bcf8f6cafe377793
honore <luishonore0511@gmail.com>
2024-04-15 15:08:04 +0200

    [0.3.0]
    feat :
    - ajout d'un bouton pour télécharger les fiches de société de stage et d'annonce de stage en PDF,
    - ajout d'un bouton qui remplit les données email du destinataire automatiquement pour envoyer un email.

--- stagemanagement/src/components/card/CardsAnnonce.js
+++ stagemanagement/src/components/card/CardsAnnonce.js
@@ -4,9 +4,12 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import CardFooter from 'react-bootstrap/esm/CardFooter';
+import Button from 'react-bootstrap/Button';
 import { URL_ALLANNONCE, URL_ALLSOCIETES } from '../../utils/Api.js';
 import CoordoneAnnonceForm from '../Form/CoordoneAnnonceForm.js';
 import AnnonceForm from '../Form/AnnonceForm.js';
+import pdfimg from '../../assets/images/file-pdf-solid-36.png';
+import jsPDF from 'jspdf';
 
 const CardsAnnonce = () => {
     const [annonces, setAnnonces] = useState([]);
@@ -31,6 +34,26 @@ const CardsAnnonce = () => {
         fetchData();
     }, []);
 
+    // generation et téléchargement du fichier PDF
+    const generatePDF = (annonce) => {
+        const doc = new jsPDF();
+        doc.text(20, 20, `Nom : ${annonce.coordones.nom}`);
+        doc.text(20, 30, `Rue : ${annonce.coordones.rue}`);
+        doc.text(20, 40, `N° : ${annonce.coordones.n}`);
+        doc.text(20, 50, `Code postal : ${annonce.coordones.code_postal}`);
+        doc.text(20, 60, `Ville : ${annonce.coordones.ville}`);
+        doc.text(20, 70, `Téléphone : ${annonce.coordones.telephone}`);
+        doc.text(20, 80, `E-mail : ${annonce.coordones.email}`);
+        doc.text(20, 90, `Site web : ${annonce.coordones.url}`);
+        doc.text(20, 100, `Secteur d'activité : ${annonce.coordones.activite}`);
+        
+        doc.text(20, 110, `Description de l'offre : ${annonce.description}`);
+        doc.text(20, 120, `Contact : ${annonce.nomContact}, ${annonce.prenomContact}`);
+        doc.text(20, 130, `E-mail de contact : ${annonce.emailContact}`);
+        doc.text(20, 140, `Téléphone de contact : ${annonce.telephoneContact}`);
+        doc.save(`${annonce.coordones.nom}-annonce.pdf`);
+    };
+
     // Mise en page
     return (
         <>
@@ -38,7 +61,7 @@ const CardsAnnonce = () => {
                 {annonces.map((annonce, idx) => (
                     <Col key={idx}>
                         <Card className='cardform'> 
-                            <Card.Header>{annonce.coordones.nom}</Card.Header>
+                            <Card.Header><b>{annonce.coordones.nom}</b></Card.Header>
                             <Card.Body className='cardsmoise'>
                                 <div className='cardsgauche'>
                                     <Card.Title>Description de l'offre : </Card.Title>
@@ -52,6 +75,9 @@ const CardsAnnonce = () => {
                             <CardFooter className='cardsmoise'>
                                 <div className='cardsgauche'><b>Date de publication : </b>{annonce.date.slice(0, 10)}</div>
                                 <div className='cardsdroite'><b>Tag : </b>{annonce.tagSection}</div>
+                                <Button variant="danger" onClick={() => generatePDF(annonce)}>
+                                    <img alt="pdf" src={pdfimg} />
+                                </Button>
                             </CardFooter>
                         </Card>
                     </Col>
--- stagemanagement/src/components/card/CardsEntreprise.js
+++ stagemanagement/src/components/card/CardsEntreprise.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from 'axios'; 
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import CardFooter from 'react-bootstrap/esm/CardFooter';
+import Button from 'react-bootstrap/Button';
+import jsPDF from 'jspdf';
 import CoordoneForm from '../Form/CoordoneForm.js';
 import NoteForm from '../Form/NoteForm.js';
 import { URL_ALLSOCIETES, URL_ALLNOTE } from '../../utils/Api.js';
+import pdfimg from '../../assets/images/file-pdf-solid-36.png';
+import emailimg from '../../assets/images/envelope-regular-36.png';
 
 const CardsEntreprise = () => {
     const [societes, setSocietes] = useState([]);
@@ -31,6 +35,28 @@ const CardsEntreprise = () => {
         fetchData();
     }, []);
 
+    // generation et téléchargement du fichier PDF
+    const generatePDF = (society) => {
+        const doc = new jsPDF();
+        doc.text(20, 20, `Nom : ${society.nom}`);
+        doc.text(20, 30, `Rue : ${society.rue}`);
+        doc.text(20, 40, `N° : ${society.n}`);
+        doc.text(20, 50, `Code postal : ${society.code_postal}`);
+        doc.text(20, 60, `Ville : ${society.ville}`);
+        doc.text(20, 70, `Téléphone : ${society.telephone}`);
+        doc.text(20, 80, `E-mail : ${society.email}`);
+        doc.text(20, 90, `Site web : ${society.url}`);
+        doc.text(20, 100, `Secteur d'activité : ${society.activite}`);
+        
+        doc.text(20, 110, `Accueil : ${society.notes.reduce((acc, note) => acc + note.accueil, 0) / society.notes.length.toFixed(1)}`);
+        doc.text(20, 120, `Charge : ${society.notes.reduce((acc, note) => acc + note.charge, 0) / society.notes.length.toFixed(1)}`);
+        doc.text(20, 130, `Implication : ${society.notes.reduce((acc, note) => acc + note.implication, 0) / society.notes.length.toFixed(1)}`);
+        doc.text(20, 140, `Lieu : ${society.notes.reduce((acc, note) => acc + note.lieu, 0) / society.notes.length.toFixed(1)}`);
+        doc.save(`${society.nom}.pdf`);
+    };
+
+    // bouton envoyer un mail
+    
 
     // Mise en page
     return (
@@ -38,7 +64,7 @@ const CardsEntreprise = () => {
             {societes.map((society, idx) => (
                 <Col key={idx}>
                     <Card className='cardform'> 
-                         <Card.Header>{society.nom}</Card.Header>
+                         <Card.Header><b>{society.nom}</b></Card.Header>
                         <Card.Body className='cardsmoise'>
                             <div className='cardsgauche'>
                                 <Card.Title>Coordonées :</Card.Title>
@@ -50,7 +76,12 @@ const CardsEntreprise = () => {
                             </div>
                         </Card.Body>
                         <CardFooter>
-
+                            <Button href={`mailto:${society.email}`}>
+                                <img alt="envoyer un E-mail" src={emailimg} />
+                            </Button>
+                            <Button variant="danger" onClick={() => generatePDF(society)}>
+                                <img alt="télécharger la fiche en pdf" src={pdfimg} />
+                            </Button>
                         </CardFooter>
                     </Card>
                 </Col>
